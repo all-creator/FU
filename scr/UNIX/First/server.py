e1d2ec59dffc8088d517f42b7ab4bbd65e927c95
all <maksim.svir.0165@gmail.com>
2021-09-16 18:26:45 +0300

    client.py and server.py

---
+++ scr/UNIX/First/server.py
@@ -0,0 +1,22 @@
+import socket
+
+sock = socket.socket()
+sock.bind(('', 9090))
+sock.listen(0)
+while True:
+	conn, addr = sock.accept()
+	print(addr)
+
+	msg = ''
+
+	while True:
+		data = conn.recv(1024)
+		if not data:
+			break
+		msg += data.decode()
+		conn.send(data)
+
+	print(msg)
+
+	if msg == "exit":
+		conn.close()
