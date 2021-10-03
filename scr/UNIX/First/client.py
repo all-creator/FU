7e38a165a089a6406d6fc1174272e524be1e4138
all <maksim.svir.0165@gmail.com>
2021-10-03 03:13:08 +0300

    Настоящий мессенджер

--- scr/UNIX/First/client.py
+++ scr/UNIX/First/client.py
@@ -1,16 +1,45 @@
 import socket
+import re
+from threading import *
 
 sock = socket.socket()
 sock.setblocking(True)
-while True:
-    sock.connect(('127.0.0.1', 9090))
+isOpen = False
 
-    msg = input()
-    sock.send(msg.encode())
 
-    data = sock.recv(1024)
+def client():
+    global isOpen
+    adr = str(input('Введите адрес сервера = '))
+    if re.search(r"^\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$", adr):
+        port = int(input('Введите порт сервера = '))
+        if 1024 < port < 65535:
+            sock.connect((adr, port))
+            isOpen = True
+            print("Подключение установленно")
+            while True:
+                msg = input()
+                sock.send(msg.encode())
 
-    if msg == "exit":
-        sock.close()
+                if msg == "exit":
+                    break
+        else:
+            print("Не верный Порт")
+    else:
+        print("Не верный IP")
 
-    print(data.decode())
+
+def server():
+    global isOpen
+    while True:
+        if isOpen:
+            data = sock.recv(1024)
+            if data is not None or data != "":
+                print(data.decode())
+
+
+if __name__ == "__main__":
+    client = Thread(target=client)
+    server = Thread(target=server)
+
+    client.start()
+    server.start()
